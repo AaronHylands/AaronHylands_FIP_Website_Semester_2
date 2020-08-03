3958281bd64176dd2d0c480bbb6041b4b3cbf537
AaronHylands <aaronhylands1@gmail.com>
2020-08-03 15:08:43 -0400

    Hamburger popup nav complete, mobile homepage styled and filled, footer still in need of completion

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log("JS file is linked");
+(() => {
+  console.log("JS file is linked");
+
+  let burgerButton = document.querySelector("#burgerButton"),
+      burgerButtonExit = document.querySelector("#burgerButtonExit"),
+      mainNav = document.querySelector("#mainNav");
+
+  function toggleNav() {
+    if (mainNav.classList.contains("hidden")) {
+    mainNav.classList.remove("hidden");
+    burgerButtonExit.classList.remove("hidden");
+  }
+    else {
+      mainNav.classList.add("hidden");
+      burgerButtonExit.classList.add("hidden");
+    }
+  }
+
+  burgerButton.addEventListener("click", toggleNav);
+  burgerButtonExit.addEventListener("click", toggleNav);
+})();
