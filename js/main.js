4ea6d507b3430b9a6142f4a6e74689c756b492b2
AaronHylands <aaronhylands1@gmail.com>
2020-08-05 11:50:30 -0400

    gallery complete, successfully coded picture swap in javascript

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@
       burgerButtonExit = document.querySelector("#burgerButtonExit"),
       returnTop = document.querySelector("#returnTop"),
       dividers = document.querySelectorAll(".divider"),
+      galleryImages = document.querySelectorAll(".galleryImage"),
+      galleryDetails = document.querySelectorAll(".galleryDetails"),
       mainNav = document.querySelector("#mainNav");
 
   function toggleNav() {
@@ -27,10 +29,34 @@
     window.scrollBy({ top: 800, behavior: 'smooth' });
   }
 
+  function animateImage() {
+    this.classList.add("galleryItemAnimate");
+
+
+
+  }
+
+  function flipImage() {
+    //let galleryItem1 = document.querySelectorAll(".galleryItem1");
+    //debugger;
+    let matches = document.querySelectorAll(`div[data-key="${this.dataset.key}"]`);
+
+    matches.forEach(match => match.classList.toggle("hidden"));
+    this.classList.remove("galleryItemAnimate")
+
+    //galleryItem1.classList.toggle("hidden");
+  }
+
   burgerButton.addEventListener("click", toggleNav);
   burgerButtonExit.addEventListener("click", toggleNav);
 
   returnTop.addEventListener("click", beamMeUpScotty);
 
   dividers.forEach(divider => divider.addEventListener("click", scrollDown));
+
+  galleryImages.forEach(galleryImage => galleryImage.addEventListener("click", animateImage));
+  galleryDetails.forEach(galleryDetail => galleryDetail.addEventListener("click", animateImage));
+
+  galleryImages.forEach(galleryImage => galleryImage.addEventListener("animationend", flipImage));
+  galleryDetails.forEach(galleryDetail => galleryDetail.addEventListener("animationend", flipImage));
 })();
