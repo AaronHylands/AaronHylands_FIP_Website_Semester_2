a724567f524d5ef199e24c7ae98fe27e8f22432e
AaronHylands <aaronhylands1@gmail.com>
2020-08-06 22:40:14 -0400

    store functionality mostly complete, moving on to contact page for now

--- js/main.js
+++ js/main.js
@@ -9,9 +9,10 @@
       galleryDetails = document.querySelectorAll(".galleryDetails"),
       storeItems = document.querySelectorAll(".storeItem"),
       storeDetails = document.querySelectorAll(".storeDetail"),
-      cartButton = document.querySelector("#shoppingCart"),
+      cartButton = document.querySelector(".shoppingCart"),
       cartContainer = document.querySelector(".cartContainer"),
       addToCart = document.querySelectorAll(".addToCart"),
+      checkoutButton = document.querySelector(".checkoutButton"),
       mainNav = document.querySelector("#mainNav");
 
   function toggleNav() {
@@ -28,15 +29,13 @@
     window.scrollBy({ top: 900, behavior: 'smooth' });
   }
 
-  function animateImage() {
-    this.classList.add("galleryItemAnimate");
-
-  }
-
   function flipImage() {
+    console.log("flipping item...")
     //let galleryItem1 = document.querySelectorAll(".galleryItem1");
-    //debugger;
+    // debugger;
     this.classList.remove("galleryItemAnimate");
+
+    // animResetImg.classList.remove("galleryItemAnimate");
     let matches = document.querySelectorAll(`div[data-key="${this.dataset.key}"]`);
     matches.forEach(match => match.classList.toggle("hidden"));
 
@@ -45,26 +44,66 @@
   }
 
   function toggleShoppingCart() {
+    cartItem = document.querySelectorAll(".cartItem");
+    cartItem.forEach(item => item.addEventListener("click", cartItemFunction));
+    console.log(cartItem);
     cartContainer.classList.toggle("hidden");
+    cartContainer.classList.toggle("flex");
   }
+
+  function animateImage() {
+    console.log("animating...");
+    // debugger;
+    this.classList.add("galleryItemAnimate");
+
+  }
+
 
   function addItemToCart() {
     //debugger;
+    this.classList.remove("galleryItemAnimate");
+    // let shopMatches = document.querySelectorAll(`div[data-key="${this.dataset.key}"]`);
+    // shopMatches.forEach(shopMatch => shopMatch.classList.toggle("hidden"));
+
     let addItem = document.querySelector(`img[data-key="${this.dataset.key}"]`);
 
     let newCartItem = document.createElement("li");
-
-    newCartItem.className = "cartItem"
+    newCartItem.dataset.key = `cart${this.dataset.key}`;
+    newCartItem.className = "cartItem";
     newCartItem.appendChild(addItem);
 
     cartContainer.appendChild(newCartItem);
 
+    cartItem = document.querySelectorAll(".cartItem");
 
   }
 
+  function checkout() {
+    location.reload();
+  }
+
+  function cartItemFunction() {
+    //console.log(`${this.dataset.key}`)
+    let   cartEdit = document.querySelector(`li[data-key="${this.dataset.key}"]`);
+    console.log(cartEdit);
+    cartEdit.classList.add("cartItemEdit");
+    cartEdit.innerHTML = `<h1 class='cartItemRemove'>Remove?</h1> <span data-key='${this.dataset.key}' class='yes'>Yes</span> <span class='no'>No</span>`;
+    let removeYes = document.querySelector(".yes"),
+        removeNo = document.querySelector(".no");
+    removeYes.addEventListener("click", removeCartItem);
+    removeNo.addEventListener("click", keepCartItem);
+
+  }
+
+  function removeCartItem() {
+    console.log(`${this.dataset.key}`);
+  }
+
+  function keepCartItem() {
+    console.log("keeping item...");
+  }
+
   addToCart.forEach(addButton => addButton.addEventListener("click", addItemToCart));
-
-
 
   burgerButton.addEventListener("click", toggleNav);
   burgerButtonExit.addEventListener("click", toggleNav);
@@ -83,5 +122,7 @@
   storeItems.forEach(storeItem => storeItem.addEventListener("animationend", flipImage));
   storeDetails.forEach(storeDetail => storeDetail.addEventListener("animationend", flipImage));
 
+  checkoutButton.addEventListener("click", checkout);
   cartButton.addEventListener("click", toggleShoppingCart);
+
 })();
